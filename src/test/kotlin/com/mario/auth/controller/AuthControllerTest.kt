3186eebd12aef70d1165db3d3ff6f8cd70f05818
mario <codemario318@gmail.com>
2023-05-03 17:06:06 +0900

    [ADD] Auth API 테스트 코드 추가

--- src/test/kotlin/com/mario/auth/controller/AuthControllerTest.kt
+++ src/test/kotlin/com/mario/auth/controller/AuthControllerTest.kt
@@ -1,17 +1,93 @@
 package com.mario.auth.controller
 
-import org.junit.jupiter.api.BeforeEach
+import com.mario.auth.domain.User
+import com.mario.auth.dto.SignupRequestDto
+import com.mario.auth.repository.UserRepository
 import org.junit.jupiter.api.Test
 
 import org.junit.jupiter.api.Assertions.*
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.http.HttpStatus
 
-class AuthControllerTest {
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class AuthControllerTest @Autowired constructor(
+    val authController: AuthController,
+    val testRestTemplate: TestRestTemplate,
+    val userRepository: UserRepository,
+) {
+    @Test
+    fun `회원 가입 성공`() {
+        val email = "codemario@gmail.com"
+        val password = "password"
+        val name = "mario"
 
-    @BeforeEach
-    fun setUp() {
+        val responseEntity = authController.signup(SignupRequestDto(email, password, name))
+
+        assertEquals(HttpStatus.OK, responseEntity.statusCode)
     }
 
     @Test
-    fun signup() {
+    fun `회원 가입 실패 - 중복`() {
+        val email = "codemario@gmail.com"
+        val password = "password"
+        val name = "mario"
+
+        val user = User(
+            null,
+            email,
+            password,
+            name
+        )
+
+        userRepository.save(user)
+
+        val responseEntity = authController.signup(SignupRequestDto(email, password, name))
+
+        assertEquals(HttpStatus.CONFLICT, responseEntity.statusCode)
+    }
+
+    @Test
+    fun `회원 가입 실패 - 이메일 누락`() {
+        val email = ""
+        val password = "password"
+        val name = "mario"
+
+        val responseEntity = authController.signup(SignupRequestDto(email, password, name))
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.statusCode)
+    }
+
+    @Test
+    fun `회원 가입 실패 - 이메일 형식 오류`() {
+        val email = "codemario318"
+        val password = "password"
+        val name = "mario"
+
+        val responseEntity = authController.signup(SignupRequestDto(email, password, name))
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.statusCode)
+    }
+
+    @Test
+    fun `회원 가입 실패 - 비밀번호 누락`() {
+        val email = "codemario318"
+        val password = ""
+        val name = "mario"
+
+        val responseEntity = authController.signup(SignupRequestDto(email, password, name))
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.statusCode)
+    }
+
+    @Test
+    fun `회원 가입 실패 - 이름 누락`() {
+        val email = "codemario318"
+        val password = "password"
+        val name = ""
+
+        val responseEntity = authController.signup(SignupRequestDto(email, password, name))
+
+        assertEquals(HttpStatus.BAD_REQUEST, responseEntity.statusCode)
     }
 }
