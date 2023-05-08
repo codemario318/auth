78f022ab3a4c7338533b67e2589e9e1106f095a3
mario <codemario318@gmail.com>
2023-05-08 17:21:32 +0900

    [UPDATE] Security 설정 변경

--- src/main/kotlin/com/mario/auth/AuthApplication.kt
+++ src/main/kotlin/com/mario/auth/AuthApplication.kt
@@ -1,9 +1,12 @@
 package com.mario.auth
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@ConfigurationPropertiesScan
+@SpringBootApplication(scanBasePackages = ["com.mario.auth.service"])
+
 class AuthApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/mario/auth/config/SecurityConfig.kt
+++ src/main/kotlin/com/mario/auth/config/SecurityConfig.kt
@@ -3,22 +3,35 @@ package com.mario.auth.config
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
-class SecurityConfig() {
+@EnableWebSecurity
+class SecurityConfig {
     @Bean
     fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
     }
 
     @Bean
-    fun filterChain(http: HttpSecurity) : SecurityFilterChain {
+    fun filterChain(http: HttpSecurity): SecurityFilterChain {
         return http
+            .headers()
+                .frameOptions()
+                .sameOrigin()
+                .and()
             .csrf().disable()
             .cors().disable()
+            .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+            .authorizeHttpRequests()
+                .anyRequest().permitAll()
+                .and()
             .build()
     }
 }
